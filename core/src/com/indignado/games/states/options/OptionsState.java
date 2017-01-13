c044a5279a88fbf6511ecf618ba873e278749cf3
Rubentxu <rubentxu74@gmail.com>
2017-01-13 23:59:41 +0100

    borrado proyecto html

--- core/src/com/indignado/games/states/options/OptionsState.java
+++ core/src/com/indignado/games/states/options/OptionsState.java
@@ -7,30 +7,39 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.ilargia.games.egdx.base.interfaces.GameState;
+import com.ilargia.games.egdx.base.interfaces.managers.ProfileManager;
 import com.ilargia.games.egdx.managers.EGAssetsManager;
+import com.ilargia.games.egdx.managers.EGPreferencesManager;
 import com.indignado.games.SMEngine;
 import com.indignado.games.Styles;
 
 public class OptionsState implements GameState {
+    private Label volumeValueSound;
+    private Label volumeValueMusic;
+    private EGPreferencesManager preferencesManager;
+    private ProfileManager<Profile> profileManager;
     private Styles styles;
     private Stage stage;
     private Table mainTable;
     private SMEngine engine;
     private EGAssetsManager assetsManager;
 
-    public OptionsState(Styles styles, SMEngine engine) {
+    public OptionsState(Styles styles, SMEngine engine, EGPreferencesManager preferencesManager,
+                        ProfileManager<Profile> profileManager) {
         this.styles = styles;
         this.engine = engine;
         this.stage = new Stage();
+        this.preferencesManager = preferencesManager;
+        this.profileManager = profileManager;
         mainTable = new Table();
         mainTable.setFillParent(true);
 
@@ -44,64 +53,113 @@ public class OptionsState implements GameState {
         InputMultiplexer multiplexer = new InputMultiplexer();
         Gdx.input.setInputProcessor(multiplexer);
         multiplexer.addProcessor(stage);
-        Gdx.app.log("Menu","LoadResources");
+        Gdx.app.log("OptionState", "LoadResources");
     }
 
     @Override
     public void init() {
-        Gdx.app.log("Menu","Init");
-        int pad = (int) (20 * Styles.ScaleUtil.getSizeRatio());
-        int pad2 = (int) (60 * Styles.ScaleUtil.getSizeRatio());
-        final TextButton btnStart = new TextButton("Comenzar", styles.skin);
-        btnStart.pad(pad, pad2, pad, pad2);
-        final TextButton btnOptions = new TextButton("Opciones", styles.skin);
-        btnOptions.pad(pad, pad2, pad, pad2);
-        final TextButton btnScores = new TextButton("Puntuaciones", styles.skin);
-        btnScores.pad(pad, pad2, pad, pad2);
-        final TextButton button3 = new TextButton("Creditos", styles.skin);
-        button3.pad(pad, pad2, pad, pad2);
-        button3.setChecked(false);
-
-        btnStart.addListener(new ClickListener() {
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Click Comenzar...");
-//                game.setNextScreen(game.selectLevelScreen.get());
-//                gameStateMachine.changeState(GameState.SHOW_NEXT_SCREEN);
-            }
-        });
-
-        btnOptions.addListener(new ClickListener() {
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Click optionScreen...");
-//                game.setNextScreen(game.optionScreen.get());
-//                gameStateMachine.changeState(GameState.SHOW_NEXT_SCREEN);
-            }
-        });
-
-        btnScores.addListener(new ClickListener() {
-            public void clicked(InputEvent event, float x, float y) {
-                System.out.println("Click highScoreScreen...");
-//                game.setNextScreen(game.highScoresScreen.get());
-//                gameStateMachine.changeState(GameState.SHOW_NEXT_SCREEN);
-            }
-        });
-
-        Label label = new Label("SUPER MARIANO", styles.skin, "header", Color.CYAN);
+        Gdx.app.log("OptionState", "Init");
+        preferencesManager.load();
+        Label label = new Label("Opciones del Juego", styles.skin, "header", Color.ORANGE);
         label.setAlignment(Align.center, Align.center);
-        mainTable.defaults().padBottom(pad);
-        if (Gdx.graphics.getHeight() < 480) mainTable.defaults().height(Gdx.graphics.getHeight() / 5f - pad);
-        mainTable.add(label);
-        mainTable.row();
-        mainTable.add(btnStart);
-        mainTable.row();
-        mainTable.add(btnOptions);
-        mainTable.row();
-        mainTable.add(btnScores);
-        mainTable.row();
-        mainTable.add(button3);
+        mainTable.setFillParent(true);
+        mainTable.defaults().pad(6 * Styles.ScaleUtil.getSizeRatio());
+        mainTable.defaults().padLeft(50 * Styles.ScaleUtil.getSizeRatio());
+        mainTable.add(label).colspan(3);
         mainTable.row();
         mainTable.setBackground(new SpriteDrawable(new Sprite((Texture) assetsManager.getTexture(Styles.MENU_BACKGROUND))));
+
+        final CheckBox musicCheckbox = new CheckBox(" Music", styles.skin);
+        musicCheckbox.align(Align.left);
+        musicCheckbox.setChecked(preferencesManager.MUSIC);
+        musicCheckbox.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                boolean enabled = musicCheckbox.isChecked();
+                preferencesManager.MUSIC = enabled;
+                assetsManager.playMusic(Styles.MUSIC_MENU);
+            }
+        });
+        mainTable.add(musicCheckbox);
+        com.badlogic.gdx.scenes.scene2d.EventListener
+
+        Slider volumeSliderMusic = new Slider(0f, 1f, 0.1f, false, styles.skin);
+        volumeSliderMusic.setValue(preferencesManager.VOL_MUSIC);
+        volumeSliderMusic.addListener((ChangeListener.ChangeEvent event) -> {
+            Slider slider = (Slider)  event.getTarget();
+            float value = slider.getValue();
+            preferencesManager.VOL_MUSIC = value;
+            updateVolumeLabelMusic();
+            return false;
+        });
+        mainTable.add(volumeSliderMusic);
+
+        volumeValueMusic = new Label(" Volume ", styles.skin);
+        updateVolumeLabelMusic();
+        mainTable.add(volumeValueMusic);
         mainTable.row();
+
+
+        final CheckBox soundCheckbox = new CheckBox(" Sound", styles.skin);
+        soundCheckbox.align(Align.left);
+        soundCheckbox.setChecked(preferencesManager.SOUND);
+        soundCheckbox.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                boolean enabled = soundCheckbox.isChecked();
+                preferencesManager.SOUND = enabled;
+
+            }
+        });
+        mainTable.add(soundCheckbox);
+
+
+        Slider volumeSliderSound = new Slider(0f, 1f, 0.1f, false, styles.skin);
+        volumeSliderSound.setValue(preferencesManager.VOL_SOUND);
+        volumeSliderSound.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Slider slider = (Slider) actor;
+                float value = slider.getValue();
+                preferencesManager.VOL_SOUND = value;
+                updateVolumeLabelSound();
+            }
+
+        });
+        mainTable.add(volumeSliderSound);
+
+        volumeValueSound = new Label(" Volume ", styles.skin);
+        updateVolumeLabelSound();
+        mainTable.add(volumeValueSound);
+        mainTable.row();
+
+        final CheckBox touchPadCheckbox = new CheckBox(" TouchPad Control", styles.skin);
+        touchPadCheckbox.align(Align.left);
+        touchPadCheckbox.setChecked(preferencesManager.TOUCH_PAD_ENABLED);
+        touchPadCheckbox.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                boolean enabled = touchPadCheckbox.isChecked();
+                preferencesManager.TOUCH_PAD_ENABLED = enabled;
+            }
+        });
+        mainTable.add(touchPadCheckbox).colspan(3);
+        mainTable.row();
+
+
+        TextButton backButton = new TextButton("Volver Menu", styles.skin);
+        backButton.pad(20);
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                preferencesManager.save();
+                game.setNextScreen(game.menuScreen.get());
+                gameStateMachine.changeState(GameState.SHOW_NEXT_SCREEN);
+            }
+        });
+
+        mainTable.add(backButton).colspan(3);
+
         this.stage.addActor(mainTable);
     }
 
@@ -132,5 +190,15 @@ public class OptionsState implements GameState {
     public void dispose() {
         stage.clear();
         stage = null;
+    }
+
+    private void updateVolumeLabelMusic() {
+        int volume = (int) (preferencesService.volMusic * 100);
+        volumeValueMusic.setText("Volume " + Integer.toString(volume));
+    }
+
+    private void updateVolumeLabelSound() {
+        int volume = (int) (preferencesService.volSound * 100);
+        volumeValueSound.setText("Volume " + Integer.toString(volume));
     }
 }
